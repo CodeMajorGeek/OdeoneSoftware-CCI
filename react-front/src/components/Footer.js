416ef1b64649a3876df40d410b6f64e971cd7dcc
Théo Colinmaire <theo.colinmaire@gmail.com>
2024-12-10 12:40:16 +0100

    Started adding media queries to CSS for responsive rendering.

--- react-front/src/components/Footer.js
+++ react-front/src/components/Footer.js
@@ -11,8 +11,8 @@ export default function Footer() {
                 <li><FontAwesomeIcon icon={ faEnvelope } /> contact@odeone-software.com</li>
                 <li><FontAwesomeIcon icon={ faMobileAlt } /> 06 74 79 36 64</li>
                 <li><FontAwesomeIcon icon={ faPhone } /> 03 88 04 20 20</li>
-                <li><a href="/">odeone-software.com</a> distribué par 67 Pesage.</li>
             </ul>
+            <p><a href="/">odeone-software.com</a> distribué par 67 Pesage.</p>
         </footer>
     )
 }
